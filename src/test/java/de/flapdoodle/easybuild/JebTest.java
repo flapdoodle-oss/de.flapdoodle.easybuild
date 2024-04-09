8d7b621aec5d3cef419e315f1900e202ab639338
Michael Mosmann <michael@mosmann.de>
2024-04-09 07:46:35 +0200

    maven setup

---
+++ src/test/java/de/flapdoodle/easybuild/JebTest.java
@@ -0,0 +1,11 @@
+package de.flapdoodle.easybuild;
+
+import org.junit.jupiter.api.Test;
+
+class JebTest {
+
+    @Test
+    void noop() {
+        Jeb testee = new Jeb();
+    }
+}
---
+++ src/test/resources/sample-project/src/test/java/org/sample/HelloWorldTest.java
@@ -0,0 +1,11 @@
+package org.sample;
+
+import org.junit.jupiter.api.Test;
+
+class HelloWorldTest {
+
+    @Test
+    void noop() {
+        
+    }
+}
